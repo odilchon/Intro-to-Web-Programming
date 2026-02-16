233f87d0a6e7442f394f81d66e727d04ee3831ab
ao12354 <ao12354@auca.kg>
2026-02-17 02:33:30 +0600

    Add Lab 7

---
+++ lab7/lab7.py
@@ -0,0 +1,20 @@
+print("Using for loop:")
+for i in range(1, 11):
+    print(i)
+
+print("\nUsing while loop:")
+count = 1
+while count <= 10:
+    print(count)
+    count += 1
+
+print("\nCharacters in 'Python':")
+for char in "Python":
+    print(char)
+
+
+print("\nEven numbers from 2 to 10:")
+number = 2
+while number <= 10:
+    print(number)
+    number += 2
